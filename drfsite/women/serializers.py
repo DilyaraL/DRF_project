b738509394ada2c63566e2dc90f49b4377aa5d7b
DilyaraL <latipova8d@gmail.com>
2023-05-29 21:19:50 +0300

    Работа с базовыми сериализаторами и представленияеми

--- drfsite/women/serializers.py
+++ drfsite/women/serializers.py
@@ -3,9 +3,15 @@ from rest_framework import serializers
 from .models import Women
 
 
-class WomenSerializer(serializers.ModelSerializer): #Сериализатор, кот. работает с Моделями
+class WomenSerializer(serializers.Serializer): #Сериализатор, кот. работает с Моделями
     # будем брать из таблицы определенные записи, представлять их в json
     # и отправлять в ответ на запрос пользователю
-    class Meta:
-        model = Women
-        fields = ('title', 'cat_id') #поля, кот. будут отправляться
+    # class Meta:
+    #     model = Women
+    #     fields = ('title', 'cat_id') #поля, кот. будут отправляться
+    title = serializers.CharField(max_length=255)
+    content = serializers.CharField()
+    time_create = serializers.DateTimeField(read_only=True)
+    time_update = serializers.DateTimeField(read_only=True)
+    is_published = serializers.BooleanField(default=True)
+    cat_id = serializers.IntegerField()
--- drfsite/women/views.py
+++ drfsite/women/views.py
@@ -1,9 +1,28 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 from .models import Women
 from .serializers import WomenSerializer
 
 
-class WomenAPIView(generics.ListAPIView):
-    queryset = Women.objects.all()# будем выводить все данные из таблицы Women
-    serializer_class = WomenSerializer
+class WomenAPIView(APIView):
+    # queryset = Women.objects.all()# будем выводить все данные из таблицы Women
+    # serializer_class = WomenSerializer
+
+    def get(self, request):
+        w = Women.objects.all()
+        return Response({'posts': WomenSerializer(w, many=True).data}) #передаем список статей, поэтому many=True
+
+    def post(self, request):
+        serializer = WomenSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True) #raise_exception чтобы клиенту была понятна ошибка
+
+        post_new = Women.objects.create(
+            title=request.data['title'],
+            content=request.data['content'],
+            cat_id=request.data['cat_id']
+        )
+
+        return Response({'post': WomenSerializer(post_new).data})
