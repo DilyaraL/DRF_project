4b05f9efe54a5c0cf737b2e0e21175df5a407907
DilyaraL <latipova8d@gmail.com>
2023-05-30 16:19:33 +0300

    Работа с методами save(), create() и update() класса Serializer

--- drfsite/women/serializers.py
+++ drfsite/women/serializers.py
@@ -15,3 +15,15 @@ class WomenSerializer(serializers.Serializer): #Сериализатор, кот
     time_update = serializers.DateTimeField(read_only=True)
     is_published = serializers.BooleanField(default=True)
     cat_id = serializers.IntegerField()
+
+    def create(self, validated_data):
+        return Women.objects.create(**validated_data) #добавляем новую запись в таблицу
+
+    def update(self, instance, validated_data): #instance - ссылка на объект модели
+        instance.title = validated_data.get("title", instance.title)
+        instance.content = validated_data.get("content", instance.content)
+        instance.time_update = validated_data.get("time_update", instance.time_update)
+        instance.is_published = validated_data.get("is_published", instance.is_published)
+        instance.cat_id = validated_data.get("cat_id", instance.cat_id)
+        instance.save()
+        return instance
--- drfsite/women/views.py
+++ drfsite/women/views.py
@@ -13,16 +13,38 @@ class WomenAPIView(APIView):
 
     def get(self, request):
         w = Women.objects.all()
-        return Response({'posts': WomenSerializer(w, many=True).data}) #передаем список статей, поэтому many=True
+        return Response({'posts': WomenSerializer(w, many=True).data})  # передаем список статей, поэтому many=True
 
     def post(self, request):
         serializer = WomenSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True) #raise_exception чтобы клиенту была понятна ошибка
+        serializer.is_valid(raise_exception=True)  # raise_exception чтобы клиенту была понятна ошибка
+        serializer.save()  # автоматически вызовет метод create
 
-        post_new = Women.objects.create(
-            title=request.data['title'],
-            content=request.data['content'],
-            cat_id=request.data['cat_id']
-        )
+        return Response({'post': serializer.data})
 
-        return Response({'post': WomenSerializer(post_new).data})
+    def put(self, request, *args, **kwargs):
+        pk = kwargs.get("pk", None)
+        if not pk:
+            return Response({'error': 'Method PUT not allowed'})
+
+        try:
+            instance = Women.objects.get(pk=pk)
+        except:
+            return Response({'error': 'Object does not exists'})
+
+        serializer = WomenSerializer(data=request.data, instance=instance)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()  # вызовет метод update, тк при создании Сериализатора было указано ДВА параметра: data, instance
+        return Response({"post": serializer.data})
+
+    def delete(self, request, *args, **kwargs):
+        pk = kwargs.get("pk", None)
+        if not pk:
+            return Response({'error': 'Method DELETE not allowed'})
+
+        try:
+            Women.objects.get(pk=pk).delete()
+        except:
+            return Response({'error': 'Object does not exists'})
+
+        return Response({"post": "delete post " + str(pk)})
