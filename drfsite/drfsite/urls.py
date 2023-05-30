ea7e1b7b17eea233af5b70f73afde9a905a364c1
DilyaraL <latipova8d@gmail.com>
2023-05-30 20:43:36 +0300

    Использование Viewsets и ModelViewSet

--- drfsite/drfsite/urls.py
+++ drfsite/drfsite/urls.py
@@ -14,13 +14,18 @@ Including anohher URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from women.views import *
+from rest_framework import routers
+
+router = routers.SimpleRouter() #создаем роутер
+router.register(r'women', WomenViewSet) # регистрируем в нем класс Viewset'a ("префикс для набора маршрута", Класс Viewset'a)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/womenlist/', WomenAPIList.as_view()),
-    path('api/v1/womenlist/<int:pk>/', WomenAPIUpdate.as_view()),
-    path('api/v1/womendetail/<int:pk>/', WomenAPIDetailView.as_view()),
+    path('api/v1/', include(router.urls)), # прописываем весь набор маршрутов, кот. был автоматически сген Роутом
+    # http://127.0.0.1:8000/women/
+    # path('api/v1/womenlist/', WomenViewSet.as_view({'get': 'list'})),
+    # path('api/v1/womenlist/<int:pk>/', WomenViewSet.as_view({'pust': 'update'})),
 ]
--- drfsite/women/views.py
+++ drfsite/women/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -7,62 +7,27 @@ from .models import Women
 from .serializers import WomenSerializer
 
 
-class WomenAPIList(generics.ListCreateAPIView):
-    """Будет возвращать и список записей по get-запросы и
-    добавлять запись по post-запросу"""
+class WomenViewSet(viewsets.ModelViewSet):
+    """"""
     queryset = Women.objects.all()
     serializer_class = WomenSerializer
 
 
-class WomenAPIUpdate(generics.UpdateAPIView):
-    """Будем возвращать измененную запись для запросов put и patch"""
-    queryset = Women.objects.all() # на самом деле тут будут отправляться не все записи, а только измененная, тк запрос ленивый
-    serializer_class = WomenSerializer
+# class WomenAPIList(generics.ListCreateAPIView):
+#     """Будет возвращать и список записей по get-запросы и
+#     добавлять запись по post-запросу"""
+#     queryset = Women.objects.all()
+#     serializer_class = WomenSerializer
+#
+#
+# class WomenAPIUpdate(generics.UpdateAPIView):
+#     """Будем возвращать измененную запись для запросов put и patch"""
+#     queryset = Women.objects.all() # на самом деле тут будут отправляться не все записи, а только измененная, тк запрос ленивый
+#     serializer_class = WomenSerializer
+#
+#
+# class WomenAPIDetailView(generics.RetrieveUpdateDestroyAPIView):
+#     """чтение, изменение и добавление отдельной записи (GET-, PUT-, PATCH- и DELETE-запросы)"""
+#     queryset = Women.objects.all()
+#     serializer_class = WomenSerializer
 
-
-class WomenAPIDetailView(generics.RetrieveUpdateDestroyAPIView):
-    """чтение, изменение и добавление отдельной записи (GET-, PUT-, PATCH- и DELETE-запросы)"""
-    queryset = Women.objects.all()
-    serializer_class = WomenSerializer
-
-# class WomenAPIView(APIView):
-#     # queryset = Women.objects.all()# будем выводить все данные из таблицы Women
-#     # serializer_class = WomenSerializer
-#
-#     def get(self, request):
-#         w = Women.objects.all()
-#         return Response({'posts': WomenSerializer(w, many=True).data})  # передаем список статей, поэтому many=True
-#
-#     def post(self, request):
-#         serializer = WomenSerializer(data=request.data)
-#         serializer.is_valid(raise_exception=True)  # raise_exception чтобы клиенту была понятна ошибка
-#         serializer.save()  # автоматически вызовет метод create
-#
-#         return Response({'post': serializer.data})
-#
-#     def put(self, request, *args, **kwargs):
-#         pk = kwargs.get("pk", None)
-#         if not pk:
-#             return Response({'error': 'Method PUT not allowed'})
-#
-#         try:
-#             instance = Women.objects.get(pk=pk)
-#         except:
-#             return Response({'error': 'Object does not exists'})
-#
-#         serializer = WomenSerializer(data=request.data, instance=instance)
-#         serializer.is_valid(raise_exception=True)
-#         serializer.save()  # вызовет метод update, тк при создании Сериализатора было указано ДВА параметра: data, instance
-#         return Response({"post": serializer.data})
-#
-#     def delete(self, request, *args, **kwargs):
-#         pk = kwargs.get("pk", None)
-#         if not pk:
-#             return Response({'error': 'Method DELETE not allowed'})
-#
-#         try:
-#             Women.objects.get(pk=pk).delete()
-#         except:
-#             return Response({'error': 'Object does not exists'})
-#
-#         return Response({"post": "delete post " + str(pk)})
