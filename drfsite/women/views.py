aea383a27da14936165d4fd49e41d67eeb641700
DilyaraL <latipova8d@gmail.com>
2023-05-31 17:59:50 +0300

    Роутеры

--- drfsite/women/views.py
+++ drfsite/women/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from rest_framework import generics, viewsets
+from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from .models import Women
+from .models import *
 from .serializers import WomenSerializer
 
 
@@ -12,6 +13,20 @@ class WomenViewSet(viewsets.ModelViewSet):
     queryset = Women.objects.all()
     serializer_class = WomenSerializer
 
+    def qet_queryset(self):
+        # Чтобы отображалось только первые 3 записи, queryset теперь можно не писать
+        pk = self.kwargs.get('pk')
+
+        if not pk:
+            return Women.objects.all()[:3]
+        # используем filter тк он тоже возвращает СПИСОК
+        return Women.objects.filter(pk=pk)
+
+    @action(methods=['get'], detail=True)  # False - возвращается список, True - одна запись
+    def category(self, request, pk=None):
+        # Будем выводить категории
+        cats = Category.objects.get(pk=pk)
+        return Response({'cats': cats.name})
 
 # class WomenAPIList(generics.ListCreateAPIView):
 #     """Будет возвращать и список записей по get-запросы и
@@ -30,4 +45,3 @@ class WomenViewSet(viewsets.ModelViewSet):
 #     """чтение, изменение и добавление отдельной записи (GET-, PUT-, PATCH- и DELETE-запросы)"""
 #     queryset = Women.objects.all()
 #     serializer_class = WomenSerializer
-
